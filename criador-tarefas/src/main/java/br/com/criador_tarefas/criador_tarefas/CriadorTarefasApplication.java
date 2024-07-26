b8881b0d1e39997aa238e76b0d590432243b6eea
possan-leonardo <leonardopossamdasilva@gmail.com>
2024-07-26 20:52:51 -0300

    Iniciando o meu projeto

---
+++ criador-tarefas/src/main/java/br/com/criador_tarefas/criador_tarefas/CriadorTarefasApplication.java
@@ -0,0 +1,15 @@
+package br.com.criador_tarefas.criador_tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriadorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriadorTarefasApplication.class, args);
+		System.out.println("Olaah");
+
+	}
+
+}
